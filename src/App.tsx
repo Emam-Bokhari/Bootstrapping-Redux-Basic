7f58bf6067e4b4239431124e2d4055add702abee
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-01 23:03:07 +0600

    feat: add increment and decrement handlers using Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Fragment } from "react/jsx-runtime";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
+
+  console.log(counter);
+
   return (
     <Fragment>
       <div>
-        <p className="text-lg font-medium">Count: 0</p>
-        <button className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded-md transition-all">
+        <p className="text-lg font-medium">Count: {counter.count}</p>
+        <button
+          onClick={handleIncrement}
+          className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded-md transition-all"
+        >
           Increment
         </button>
-        <button className="bg-red-500  hover:bg-red-600 px-4 py-2 rounded-md text-white ml-2">
+        <button
+          onClick={handleDecrement}
+          className="bg-red-500  hover:bg-red-600 px-4 py-2 rounded-md text-white ml-2"
+        >
           Decrement
         </button>
       </div>
