44e5bbccbe3d4160137062a1c3262672189a82fc
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-01 20:34:43 +0600

    init: initialize project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sequi
+        temporibus, a quam sapiente laborum ratione eveniet qui officia
+        inventore, vero labore esse laudantium sunt. Officia inventore deleniti
+        numquam nesciunt.
+      </p>
+    </div>
+  );
+};
+
+export default App;
