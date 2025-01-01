9643f9bb0096ac59f3baee8bcc5f57d54632280a
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-01 22:14:02 +0600

    feat: add counter UI with increment and decrement buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Fragment } from "react/jsx-runtime";
+
 const App = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam sequi
-        temporibus, a quam sapiente laborum ratione eveniet qui officia
-        inventore, vero labore esse laudantium sunt. Officia inventore deleniti
-        numquam nesciunt.
-      </p>
-    </div>
+    <Fragment>
+      <div>
+        <p className="text-lg font-medium">Count: 0</p>
+        <button className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded-md transition-all">
+          Increment
+        </button>
+        <button className="bg-red-500  hover:bg-red-600 px-4 py-2 rounded-md text-white ml-2">
+          Decrement
+        </button>
+      </div>
+    </Fragment>
   );
 };
 
