c8c56ad5968e253cd7db2f5559de9feaae574a4a
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-02 11:58:34 +0600

    feat: integrate task slice into existing Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice"
+import taskSliceReducer from "./features/task/taskSlice"
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        task: taskSliceReducer,
     }
 })
 
